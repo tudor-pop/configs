b400584bc0c72e6be3424601300d40be001b42d9
Tudor <tudor14pop@gmail.com>
2020-02-28 20:33:30 +0200

    Update osx_setup.sh
    install useful packages

--- osx_setup.sh
+++ osx_setup.sh
@@ -1,1 +1,35 @@
 
+#!/bin/bash
+
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# install zsh
+brew install zsh
+brew install starship
+brew install wget
+brew install exa
+brew install fzf
+brew install httpie
+
+# To install useful key bindings and fuzzy completion:
+$(brew --prefix)/opt/fzf/install
+
+# change the default shell of the root user to zsh with the chsh command below.
+curl -L git.io/antigen > ~/antigen.zsh
+cp .zshrc ~
+
+echo "alias ll=\"exa -abghHlS -L 1\"" >> ~/.profile
+
+# Vim setup
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+cp .vimrc ~/
+
+vim +PlugInstall +qall > /dev/null
+
+sudo chsh -s "$(command -v zsh)" "${USER}"
+echo $SHELL
+#reload terminal
+source ~/.zshrc
+source ~/.profile
