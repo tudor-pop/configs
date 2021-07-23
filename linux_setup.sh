7e6a29a917dfd5b4138c9c1470ca3df50a7890a6
Tudor <tudor14pop@gmail.com>
2021-07-23 17:18:15 +0300

    Update linux_setup.sh

--- linux_setup.sh
+++ linux_setup.sh
@@ -62,4 +62,10 @@ sudo usermod -aG docker $USER
 # configure docker to start on reboot
 sudo systemctl enable docker.service
 sudo systemctl enable containerd.service
+# compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+
 
